1f04a858d603f8036c6b4821ec8cfb3501f8fb8e
Sharad Varshney <33467893+sgvarsh@users.noreply.github.com>
2019-03-06 15:36:20 -0800

    Update app.py

--- app.py
+++ app.py
@@ -1,34 +1,33 @@
 #!/usr/bin/env python
 """
 Program: app.py
-Purpose: Used for running assignment model and smart routing
+Purpose: Used for running Supermarket optimization tool
 Author:  Sharad Varshney
 Created: Jan 29th, 2019
 """
 import logging
+from optparse import OptionParser
+
 from supermarket_optimization import SupermarketOptimization
 
+
 if __name__ == "__main__":
-    logging.basicConfig(format='%(levelname)s|%(asctime)s|%(filename)s -> %(funcName)s[%('
-                               'lineno)d] |%(name)s|%(message)s')
+    #if needed we can create a microservice out of this by adding micro webserver Flask here.
+    logging.basicConfig()
     logger = logging.getLogger()
     logger.setLevel(logging.DEBUG)
     apriori = SupermarketOptimization()
-    filename = './retail_25k.dat'
-    filename = './sample.dat'
-    minsupport = 4.0
+    default_filename = './sample.dat'
+    default_support = 4.0
+    default_output = './frequent_sets_25K_new.txt'
 
-    list = [[1, 2], [3, 2]]
-    c1 = map(frozenset, list)
-    for key, value in c1:
-        print(key)
-        print(value)
-        print('New line')
-    data = apriori.load_data(filename)
-    run = apriori.apriori_run()
-    logging.debug('FInalize the run')
+    parser = OptionParser(usage="Program usage: %prog -f filename -s minimum support")
+    filename, minsupport, output = SupermarketOptimization.get_inputs_from_cmd(parser,
+                                        default_filename,default_support, default_output)
 
 
-
-
-
+    dataset = apriori.load_data(filename)
+    scanned_reduced_list, support_data = apriori.apriori_run(minsupport)
+    logging.debug('Write output of the run to output file')
+    SupermarketOptimization.write_output(scanned_reduced_list, support_data, output)
+    
